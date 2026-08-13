0151dacf36661bf77c2f693b72e880261aa3316b
Hugo Gruson <git@hugogruson.fr>
2026-08-13 18:55:27 +0200

    Reuse .remart_lookup_id() in getGene()

--- R/getGene.R
+++ R/getGene.R
@@ -31,13 +31,7 @@ getGene <- function(
     id <- list(id)
   }
 
-  res <- httr2::request("https://rest.ensembl.org") |> 
-    httr2::req_url_path("/lookup/id/") |>
-    httr2::req_method("POST") |>
-    httr2::req_user_agent(REMART_USER_AGENT) |>
-    httr2::req_body_json(list(ids = id)) |>
-    httr2::req_perform() |> 
-    httr2::resp_body_json(simplify = TRUE)
+  res <- .remart_lookup_id(id, expand = FALSE)
   
   warning("'band' column information is not available from the Ensembl REST API, it will be filled with NA values.")
   df <- lapply(res, function(x) {
