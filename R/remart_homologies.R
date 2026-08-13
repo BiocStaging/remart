da9d63a910a410f061c2870be7ae63caeef5aa0f
Hugo Gruson <git@hugogruson.fr>
2026-08-13 15:24:21 +0200

    Move .remart_homologies() to its own file

---
+++ R/remart_homologies.R
@@ -0,0 +1,17 @@
+#' Fetch orthologues of `id` in `target_species` from the REST `homology/id`
+#' endpoint
+#' @noRd
+.remart_homologies <- function(id, species, target_species) {
+  resp <- httr2::request("https://rest.ensembl.org") |>
+    httr2::req_url_path(sprintf("/homology/id/%s/%s", species, id)) |>
+    httr2::req_url_query(
+      type = "orthologues",
+      target_species = target_species,
+      `content-type` = "application/json"
+    ) |>
+    httr2::req_user_agent("remart R package") |>
+    httr2::req_perform() |>
+    httr2::resp_body_json(simplifyVector = FALSE)
+
+  resp$data[[1]]$homologies
+}
