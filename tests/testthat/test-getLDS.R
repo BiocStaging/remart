3c255bc8c07e05ec3ab3bb9c098ee637fac34a4f
Hugo Gruson <git@hugogruson.fr>
2026-08-14 11:45:29 +0200

    Add tests for getLDS()

---
+++ tests/testthat/test-getLDS.R
@@ -0,0 +1,21 @@
+test_that("getLDS() works with homologies", {
+  res <- getLDS(
+    attributes = c("ensembl_gene_id", "hgnc_symbol", "external_gene_name"),
+    filters = "ensembl_gene_id",
+    values = c("ENSG00000157764", "ENSG00000004939"),
+    attributesL = c("ensembl_gene_id"),
+    speciesL = "mouse"
+  ) |>
+    expect_no_error() |>
+    expect_no_warning()
+
+  expect_named(
+    res,
+    c(
+      "ensembl_gene_id",
+      "hgnc_symbol",
+      "external_gene_name",
+      "ensembl_gene_id.1"
+    )
+  )
+})
